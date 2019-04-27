b6dbe93b0822bbffc4f3c8a4d28fedab3a1b5fbb
Harshit Sethi <hsethi2709@gmail.com>
2019-04-27 21:58:21 +1000

    Displaying highlighted text from the page in the popup

--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
-function getSelectionText() {
+function getText() {
+chrome.tabs.executeScript( {
+	code: "window.getSelection().toString();"
+}, function(selection) {
+	document.getElementById("sentence").innerHTML = selection[0];
+});
+}
 
-}	
-document.getElementById("fact_check").addEventListener('click', getParsedPage());
+document.getElementById('fact_check').addEventListener('click',getText);
+
