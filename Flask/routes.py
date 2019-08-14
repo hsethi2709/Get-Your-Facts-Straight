b3f824ad1a4d72f8f05f8e2a530c2a9a5da29c4f
Harshit Sethi <hsethi2709@gmail.com>
2019-08-14 15:16:43 +1000

    Send message from extension to the flask server

--- Flask/routes.py
+++ Flask/routes.py
@@ -1,5 +1,8 @@
 from Flask import app
+from flask import request
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 def index():
-    return "Hello, World!"
+    print(request.args.get('sentence'))
+    sentence = request.args.get('sentence')
+    return sentence
--- popup.js
+++ popup.js
@@ -2,6 +2,9 @@ function getText() {
 chrome.tabs.executeScript( {
 	code: "window.getSelection().toString();"
 }, function(selection) {
+
+	url='http://localhost:5000/index?sentence='.concat(selection[0])
+	fetch(url);
 	document.getElementById("sentence").innerHTML = selection[0];
 });
 }
