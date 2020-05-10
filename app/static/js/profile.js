db395430f54efa6bbe89bb048fd4d3ac02fdf4bb
Harshit Sethi <harshit@cognitiveview.com>
2020-05-10 17:20:09 +1000

    security for dashboard

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -77,5 +77,26 @@ function getPID(){
     );
     }
 
-getPID();
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }  
+    
+    if (window.location.href != "https://www.getfactcheck.me/dashboard_login"){
+    login = getCookie("dashboard_login")
+    if (login!= null)
+    {
+        getPID();
+    }
+    else{
+        window.location.assign("https://www.getfactcheck.me/dashboard_login");
+    }
+    }
+
 
