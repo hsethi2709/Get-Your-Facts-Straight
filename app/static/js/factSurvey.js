f0aa9110dc2aea304154ff1aa8a6f232ecfa0d63
Harshit Sethi <harshit@cognitiveview.com>
2020-08-19 19:00:18 +1000

    spinner added

--- app/static/js/factSurvey.js
+++ app/static/js/factSurvey.js
@@ -92,6 +92,7 @@ function submitConfidence(evt){
 			"confidence_scale": confidence_scale
 		};
 		// url='http://localhost:5000/addSentenceToClient' 
+		spinner.removeAttribute("hidden");
 		url='https://www.getfactcheck.me/addSentenceToClient'
 		fetch(url, {
 			method:'post',
@@ -101,6 +102,7 @@ function submitConfidence(evt){
 			body: JSON.stringify(payload)
 		}).then(function(response) {
 			if (response.status == 200) {
+				spinner.setAttribute("hidden", "");
 				count += 1
 				console.log("Count:", count)
 				final_count += 1
